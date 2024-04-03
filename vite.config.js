4a7abd4bb34bdf4bb1a0da31aa79ca1d52f36c16
kathosseni <ossenimalikath@gmail.com>
2024-04-03 03:52:18 +0200

    Vite config update with dist

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build:{
-    outDir: "build"
-  },
+  build: { manifest: true, outDir: './dist'},
+  base:  '/',
+  root: './src',
 })
